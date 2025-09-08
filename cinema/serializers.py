9ec74e997e3391d8485f70f266774b1d142dd94a
Ermolenko Andrey <f1nd6r@gmail.com>
2025-09-08 20:48:12 +0300

    Solution2

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -10,7 +10,7 @@ class MovieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = "__all__"
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
--- cinema/urls.py
+++ cinema/urls.py
@@ -4,6 +4,6 @@ from cinema.views import movie_detail, movie_list
 app_name = "cinema"
 
 urlpatterns = [
-    path("movie/", movie_list, name="movie_list"),
-    path("movie/<int:pk>/", movie_detail, name="movie_detail"),
+    path("movies/", movie_list, name="movie_list"),
+    path("movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
