4716428a72813a32f0cea21209d22267f05acf2d
Ermolenko Andrey <f1nd6r@gmail.com>
2025-09-09 12:18:06 +0300

    Solution4

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,8 +3,8 @@ from cinema.views import movie_detail, movie_list
 
 app_name = "cinema"
 
+
 urlpatterns = [
     path("movies/", movie_list, name="movie_list"),
     path("movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
-
